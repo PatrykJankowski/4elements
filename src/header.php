1ad4a03e0e2b611444934b24f9c805e9e565123b
PatrykJankowski <patryk.jankowski@protonmail.com>
2020-09-09 17:28:55 +0200

    slider created

--- src/header.php
+++ src/header.php
@@ -46,8 +46,66 @@
 
     <?php if (is_front_page() && !is_home()) { ?>
         <header id="header">
-            <section id="branding">
-                <?php dynamic_sidebar('slogan'); ?>
-            </section>
+            <div class="slider">
+                <img class="slider__img" src="/wp-content/themes/4elements/img/nauka_plywania_dla_dzieci@1920.jpg">
+                <img class="slider__img" src="/wp-content/themes/4elements/img/bg.jpg">
+
+
+                <div class="slider__caption">
+                    <?php dynamic_sidebar('slogan'); ?>
+                </div>
+
+                <div class="slider__arrows">
+                    <button class="slider__arrow slider__arrow--left" onclick="plusDivs(-1)">&#10094;</button>
+                    <button class="slider__arrow slider__arrow--right" onclick="plusDivs(1)">&#10095;</button>
+                </div>
+
+                <div class="slider__badges">
+                    <span class="slider__badge" onclick="currentDiv(1)"></span>
+                    <span class="slider__badge" onclick="currentDiv(2)"></span>
+                </div>
+            </div>
         </header>
+
     <?php } ?>
+
+    <script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+      showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+      let i;
+      let x = document.getElementsByClassName("slider__img");
+      let dots = document.getElementsByClassName("slider__badge");
+      let slogan = document.getElementsByClassName("slider__slogan");
+
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+
+      for (i = 0; i < x.length; i++) {
+        x[i].style.opacity = "0";
+      }
+
+      for (i = 0; i < slogan.length; i++) {
+        slogan[i].style.opacity = "0";
+      }
+
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" slider__badge--active", "");
+      }
+
+      x[slideIndex-1].style.opacity = "1";
+      x[slideIndex-1].style.transition = "all 3s";
+      slogan[slideIndex-1].style.opacity = "1";
+      slogan[slideIndex-1].style.transition = "all 3s";
+      dots[slideIndex-1].className += " slider__badge--active";
+    }
+    </script>
