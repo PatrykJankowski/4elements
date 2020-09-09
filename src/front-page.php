9f168dabb5cf225a30e8a47ff85320fcd429c209
PatrykJankowski <patryk.jankowski@protonmail.com>
2020-09-09 17:30:57 +0200

    slider update

--- src/front-page.php
+++ src/front-page.php
@@ -235,4 +235,45 @@
     </div>
 </section>
 
+<script defer>
+    let slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+      showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+      let i;
+      let x = document.getElementsByClassName("slider__img");
+      let dots = document.getElementsByClassName("slider__badge");
+      let slogan = document.getElementsByClassName("slider__slogan");
+
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+
+      for (i = 0; i < x.length; i++) {
+        x[i].style.opacity = "0";
+      }
+
+      for (i = 0; i < slogan.length; i++) {
+        slogan[i].style.opacity = "0";
+      }
+
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" slider__badge--active", "");
+      }
+
+      x[slideIndex-1].style.opacity = "1";
+      x[slideIndex-1].style.transition = "all 3s";
+      slogan[slideIndex-1].style.opacity = "1";
+      slogan[slideIndex-1].style.transition = "all 3s";
+      dots[slideIndex-1].className += " slider__badge--active";
+    }
+</script>
+
 <?php get_footer(); ?>
